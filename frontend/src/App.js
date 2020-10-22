61222abf0727bea5ced1d6196aa688a4b1f17b2c
Suraj Bolanavar <svbsuraj@gmail.com>
2020-10-22 09:25:55 +0530

    Homescreen implementation with hardcoded list of products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
-          <h1>Welcome to easybuy</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
