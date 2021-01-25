5de079fe09ed88e460df9f28bcb316953cf25316
Suraj Bolanavar <svbsuraj@gmail.com>
2021-01-26 04:48:18 +0530

    commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ router.get(
   '/',
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+
     res.json(products);
   })
 );
@@ -18,7 +19,8 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ msg: 'Product not found' });
+      res.status(404);
+      throw new Error('Product not found');
     }
   })
 );
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const products = require('./data/products');
 const connectDB = require('./config/db');
 const colors = require('colors'); // need tpackage to show different colors underlines for logs
 const productRoutes = require('./routes/productRoutes');
+const errorMiddleware = require('./middlewares/errorMiddlewares');
 dotenv.config();
 
 const app = express();
@@ -14,15 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
-
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(errorMiddleware.notFound);
+app.use(errorMiddleware.errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
